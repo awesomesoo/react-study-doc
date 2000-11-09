79dc50f832c4157a558772111570a3c912e56b80
uxuisooa <sooa0513@naver.com>
2000-11-09 11:27:05 +0000

    테스트용 프로젝트 구현

---
+++ project7/src/componentsReview/sampleTest/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import styles from './TodoList.module.css';
+
+const TodoList = ({ data, onDel, onToggle }) => {
+  return (
+    <ul className={styles.TodoList}>
+      {data.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onDel={onDel}
+          onToggle={onToggle}
+          styles={styles}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
