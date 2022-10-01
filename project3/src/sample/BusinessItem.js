026199f85391ce864ff530fe64b7a9d6fdb256d4
uiuxsooa <sooa0513@naver.com>
2022-10-01 10:50:41 +0900

    구조분해할당으로 정리

--- project3/src/sample/BusinessItem.js
+++ project3/src/sample/BusinessItem.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const BusinessItem = ({ item }) => {
+  const { imgurl, kor, eng, des } = item; //구조분해할당
   return (
     <li>
       <a href="#">
-        <div>
+        {/* //구조분해할당 */}
+        {/* <div> 
           <img src={item.imgurl} alt={item.kor} />
         </div>
         <h3>
           {item.kor} <span>{item.eng}</span>
         </h3>
-        <p>{item.des}</p>
+        <p>{item.des}</p> */}
+        <div>
+          <img src={imgurl} alt={kor} />
+        </div>
+        <h3>
+          {kor} <span>{eng}</span>
+        </h3>
+        <p>{des}</p>
       </a>
     </li>
   );
