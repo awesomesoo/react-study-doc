63b50524dad798fa0fd8c00b875a6df6e979d2a6
uiuxsooa <sooa0513@naver.com>
2022-10-01 16:52:25 +0900

    자식컴포넌트간의값을 전달할 경우 부모가 데이터 관리(3)

--- project3/src/sample2/Main.js
+++ project3/src/sample2/Main.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Step1 from './Step1';
 import './style.css';
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div className="wrap">
+      <Step1 />
+      {/* <Step2 />
+      <Step3 />
+      <Step4 /> */}
+    </div>
+  );
 };
 
 export default Main;
