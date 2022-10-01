79e575339337168222814725085cd1705181d4fd
uiuxsooa <sooa0513@naver.com>
2022-10-01 13:04:23 +0900

    체크박스 상태에 따른 보이고 숨기기

--- project3/src/App.js
+++ project3/src/App.js
@@ -4,7 +4,8 @@
 // import Test3 from './components/Test3';
 // import Test4 from './components/Test4';
 // import Test5 from './components/Test5';
-import Test6 from './components/Test6';
+// import Test6 from './components/Test6';
+import Test7 from './components/Test7';
 
 const App = () => {
   return (
@@ -13,7 +14,8 @@ const App = () => {
       <Test3 />
       <Test4 /> */}
       {/* <Test5 /> */}
-      <Test6 />
+      {/* <Test6 /> */}
+      <Test7 />
     </div>
   );
 };
--- project3/src/components/Test7.js
+++ project3/src/components/Test7.js
@@ -1,7 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+// 1. 체크박스 true 상자 보이고 숨기기 - 삼항, && 이용
+// 2. false이면 보이기 true이면 숨기기
 const Test7 = () => {
-  return <div></div>;
+  const [isChek, setIsChek] = useState(false);
+  const changeInput = (e) => {
+    const { checked } = e.target;
+    setIsChek(checked);
+  };
+  const css1 = {
+    width: 100,
+    height: 100,
+    background: 'tomato',
+    marginBottom: 20,
+    textAlign: 'center',
+    lineHeight: 2,
+    fontSize: 50,
+  };
+  return (
+    <div style={{ margin: 20 }}>
+      <input
+        type="checkbox"
+        name="chk1"
+        checked={isChek}
+        onChange={changeInput}
+      />
+      보이기 / 숨기기
+      {isChek === true ? <div style={css1}>1</div> : <div style={css1}>2</div>}
+    </div>
+  );
 };
 
 export default Test7;
