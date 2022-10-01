e9171c056ccfdfbb71bba940d00ca05afcce1056
uiuxsooa <sooa0513@naver.com>
2022-10-01 14:59:44 +0900

    자식컴포넌트간의값을 전달할 경우 부모가 데이터 관리

--- project3/src/App.js
+++ project3/src/App.js
@@ -3,10 +3,8 @@
 // import Test2 from './components/Test2';
 // import Test3 from './components/Test3';
 // import Test4 from './components/Test4';
-// import Test5 from './components/Test5';
-// import Test6 from './components/Test6';
-// import Test7 from './components/Test7';
-import Test8 from './components/Test8';
+
+import Test9 from './components/Test9';
 
 const App = () => {
   return (
@@ -14,7 +12,7 @@ const App = () => {
       {/* <Test2 />
       <Test3 />
       <Test4 /> */}
-      <Test8 />
+      <Test9 />
     </div>
   );
 };
--- project3/src/components/Test9.js
+++ project3/src/components/Test9.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const Display = ({ ani }) => {
+  return (
+    <>
+      <h3>Display 컴포넌트</h3>
+      <h2>내가 좋아하는 동물은 "{ani}" 입니다.</h2>
+    </>
+  );
+};
+
+const Animal = ({ changeInput, ani }) => {
+  return (
+    <>
+      <h3>Animal 컴포넌트</h3>
+      <p>
+        <label>동물: </label>
+        <input type="text" value={ani} onChange={changeInput} />
+        <span style={{ marginLeft: 15, color: 'hotpink' }}>{ani}</span>
+      </p>
+    </>
+  );
+};
+
+const Name = () => {
+  const [text, setText] = useState('');
+
+  const changeInput = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+
+  return (
+    <>
+      <h3>Name 컴포넌트</h3>
+      <p>
+        <label>이름: </label>
+        <input type="text" value={text} onChange={changeInput} />{' '}
+        <span style={{ marginLeft: 15, color: 'hotpink' }}>{text}</span>
+      </p>
+    </>
+  );
+};
 
 const Test9 = () => {
-  return <div></div>;
+  const [ani, setAni] = useState('고양이');
+
+  const changeInput = (e) => {
+    const { value } = e.target;
+    setAni(value);
+  };
+
+  return (
+    <div style={{ margin: 30 }}>
+      <Name />
+      <Animal changeInput={changeInput} ani={ani} />
+      <Display ani={ani} />
+    </div>
+  );
 };
 
 export default Test9;
+/* 
+인풋에 글씨를 입력 -> 상태값이 필요
+인풋에 글씨가 바뀜 -> 이것을 저장할 변수를 만들어줌
+변수를 
+*/
+/* 
+ani 값이 display에 전달되어야하니까
+
+
+*/
