3b498d0d09b3a1e59f7683e7fb99f0db1ed863e1
uiuxsooa <sooa0513@naver.com>
2022-10-01 11:16:45 +0900

    카운터 만들기

--- project3/src/components/Test1.js
+++ project3/src/components/Test1.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 
 const Test1 = () => {
   const [count, setCount] = useState(0);
+  //  const [count, setCount] = useState(초기값);
 
   const increment = () => {
     setCount(count + 1);
+    //   count + 1 해주고 setCount 넘겨라
     console.log(count);
   };
   const decrement = () => {
