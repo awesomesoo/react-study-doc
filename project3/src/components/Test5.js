82a9a634fd9d78645d284e36afedd16c96f1c0fa
uiuxsooa <sooa0513@naver.com>
2022-10-01 12:31:29 +0900

    select를 선택하고 버튼을 눌렀을 때  지정한 색으로 변경하기

--- project3/src/components/Test5.js
+++ project3/src/components/Test5.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 
 const Test5 = () => {
   const [color, setColor] = useState('');
+  const [text, setText] = useState(''); // 1. 임시로 text라는 변수에 담고
 
   const changeInput = (e) => {
     const { value } = e.target;
-    setColor(value);
+    // setColor(value);
+    setText(value); // 2. setText를 불러와준다
   };
+
+  const onShow = () => {
+    setColor(text);
+  };
+
   return (
     <div>
       <h2 style={{ color: color }}>오늘은 토요일 입니다.</h2>
@@ -16,6 +23,8 @@ const Test5 = () => {
         <option value="blue">blue</option>
         <option value="pink">pink</option>
       </select>
+      {/* 변경을 눌렀을 때 색 바뀌기 */}
+      <button onClick={onShow}>변경</button>
     </div>
   );
 };
