12dca05554e40fc4d1a1f807d1837467f3c5a8b2
uiuxsooa <sooa0513@naver.com>
2022-10-15 14:34:06 +0900

    삭제, 추가, 수정 콜백으로 값을 전달

--- project4/src/components/Test5.js
+++ project4/src/components/Test5.js
@@ -2,19 +2,37 @@ import React, { useState } from 'react';
 import Test5Sub from './Test5Sub';
 
 const dataList = [
-  { id: 1, text: '일 입니다' },
-  { id: 2, text: '이 입니다' },
-  { id: 3, text: '삼 입니다' },
-  { id: 4, text: '사 입니다' },
-  { id: 5, text: '오 입니다' },
+  { id: 1, text: '일' },
+  { id: 2, text: '이' },
+  { id: 3, text: '삼' },
+  { id: 4, text: '사' },
+  { id: 5, text: '오' },
 ];
 
 const Test5 = () => {
   const [data, setData] = useState(dataList);
+
+  const onDel = () => {
+    alert('삭제');
+  };
+  const onAdd = () => {
+    alert('추가');
+  };
+  const onMod = () => {
+    alert('수정');
+  };
+
   return (
     <div>
       <h2>삭제, 추가, 수정</h2>
-      <Test5Sub />
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            {item.id} / {item.text}
+          </li>
+        ))}
+      </ul>
+      <Test5Sub onDel={onDel} onAdd={onAdd} onMod={onMod} />
     </div>
   );
 };
--- project4/src/components/Test5Sub.js
+++ project4/src/components/Test5Sub.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
-const Test5Sub = () => {
+// 주의: { onDel, onAdd, onMod }에 괄호{}가 없으면 매개변수로 인식함
+const Test5Sub = ({ onDel, onAdd, onMod }) => {
   return (
     <div>
       <p>
-        <button>삭제</button>
-        <button>추가</button>
-        <button>수정</button>
+        <button>id:1 삭제</button>
+        <button>새 '육' 추가</button>
+        <button>'사' 수정</button>
       </p>
     </div>
   );
