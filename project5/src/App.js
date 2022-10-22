715512d4f1cc94a171cb0177d6f3815448e80e85
uiuxsooa <sooa0513@naver.com>
2022-10-22 15:44:59 +0900

    useEffect 사용해보기 두번째-포커스 한번만 실행

--- project5/src/App.js
+++ project5/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import './assets/api/css/reset.css';
 // import Friend from './sample2Friend/Friend';
-import Test1useEffect from './test/Test1useEffect';
+import Test2useEffect from './test/Test2useEffect';
 
 function App() {
   return (
     <>
-      <Test1useEffect />
+      <Test2useEffect />
     </>
   );
 }
