e99372f30bc4f0b660a7be9461131d2accbd8306
uiuxsooa <sooa0513@naver.com>
2022-10-22 12:43:58 +0900

    체크박스 클릭시 보이기/숨기기 토글

--- project5/src/sample2Friend/Friend.js
+++ project5/src/sample2Friend/Friend.js
@@ -6,6 +6,8 @@ import './assets/style.css';
 
 const Friend = () => {
   const [data, setData] = useState(dataList);
+  const [isChk, setIsChk] = useState(false);
+
   // 삭제
   const onDel = (id) => {
     setData(data.filter((item) => item.id !== id));
@@ -18,19 +20,26 @@ const Friend = () => {
   const onReset = () => {
     setData(dataList);
   };
+  // 체크박스 클릭시 보이기/숨기기 토글
+  const changeInput = (e) => {
+    const { checked } = e.target;
+    setIsChk(checked);
+  };
 
   return (
     <div className="wrap">
       <h1>친구들 총 인원 : {data.length}</h1>
       <p className="chk">
-        <input type="checkbox" name="" /> 보이기 / 숨기기
+        <input type="checkbox" checked={isChk} onChange={changeInput} /> 보이기
+        / 숨기기
       </p>
       <FriendList data={data} onDel={onDel} />
       <p className="btn">
         <button onClick={onRemove}>모두삭제</button>
         <button onClick={onReset}>초기복구</button>
       </p>
-      <FriendInput />
+      {isChk && <FriendInput />}
+      {/* {isChk ? <FriendInput />} : null */}
     </div>
   );
 };
