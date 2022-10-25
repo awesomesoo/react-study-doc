75c5f4fbb96bcf3d496d50cbe9a5b593640585d8
uiuxsooa <sooa0513@naver.com>
2022-10-25 16:28:50 +0900

    고객명단 등록, 수정, 삭제, 조회 프로젝트 작업중

--- project5/src/sample3useEffect/AddUser.js
+++ project5/src/sample3useEffect/AddUser.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
-const AddUser = () => {
-  return <div>AddUser</div>;
+const AddUser = ({ onAdd }) => {
+  const [user, setUser] = useState({ text: '', addr: '' });
+  const textRef = useRef(null);
+  const { text, addr } = user;
+
+  const changeInput = (e) => {
+    const { value, name } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault(); // 새로고침 기능 차단.
+    if (!text || !addr) return; // 인풋이 하나라도 빈 값이면 안된다는 의미.
+    onAdd(user); // 추가를 해준다.
+    setUser({ text: '', addr: '' }); // 인풋을 빈값으로 만들자.
+    textRef.current.focus(); // 포커스하겠다.
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h2>작은아씨들 출연진 추가</h2>
+      <p>
+        <label>이름</label>
+        <input
+          type="text"
+          name="text"
+          value={text}
+          onChange={changeInput}
+          ref={textRef}
+        />
+      </p>
+      <p>
+        <label>주소</label>
+        <input type="text" name="addr" value={addr} onChange={changeInput} />
+      </p>
+      <p>
+        <button type="submit">추가</button>
+      </p>
+    </form>
+  );
 };
 
 export default AddUser;
