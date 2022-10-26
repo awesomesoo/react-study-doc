327a80477e18dbdb2fa224761495fc5bfcd20beb
uiuxsooa <sooa0513@naver.com>
2022-10-27 01:35:17 +0900

    갤러리 이미지 리스트 노출

--- project5review/src/gallery/component/Gallery.js
+++ project5review/src/gallery/component/Gallery.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import GalleryBig from './GalleryBig';
 import GalleryList from './GalleryList';
+import dataList from '../../api/catData';
 
 const StyledGallery = styled.div`
   width: 600px;
@@ -9,11 +11,16 @@ const StyledGallery = styled.div`
 `;
 
 const Gallery = () => {
+  const [data, setData] = useState(dataList);
+  const [cat, setCat] = useState(data[0]);
+
+  const onShow = (id) => {};
+
   return (
     <div>
       <StyledGallery>
-        <GalleryBig />
-        <GalleryList />
+        <GalleryBig cat={cat} />
+        <GalleryList data={data} onShow={onShow} />
       </StyledGallery>
     </div>
   );
@@ -22,6 +29,31 @@ const Gallery = () => {
 export default Gallery;
 
 /* 
+갤러리를 만들 컴포넌트 준비
+Gallery : 큰 틀
+GalleryBig : 이미지 크게 보기
+GalleryList : 이미지 리스트
+GalleryItem : 이미지 리스트 아이템
+
+useState 사용하여 api 가져오기.
+GalleryList 에 data 보내주기.
+GalleryList 에서 data 파라미터로 가져오기.
+ul안에 li를 .map()으로 불러오기.
+
+
+
+
+
+const [cat, setCat] = useState(data[0]);
+
+
+
+
+
+
+
+
+
 
 
 */
