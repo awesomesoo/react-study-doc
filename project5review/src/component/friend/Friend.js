b3b9065066b6b54840d0a92256af15f5441c3322
uxuisooa <sooa0513@naver.com>
2022-10-31 02:42:18 +0900

    프렌즈 컴포넌트 작업중

--- project5review/src/component/friend/Friend.js
+++ project5review/src/component/friend/Friend.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import FriendList from './FriendList';
+import styled from 'styled-components';
+
+const StyledFriend = styled.div`
+  .wrap {
+    width: 400px;
+    margin: 20px auto;
+    border: 1px solid #999;
+    padding: 25px;
+    position: relative;
+  }
+  h1 {
+    font-size: 25px;
+    margin-bottom: 15px;
+  }
+  .chk {
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    border-radius: 20px;
+    display: block;
+    background: #333;
+    color: #fff;
+    width: 100px;
+    text-align: center;
+    padding: 10px 0;
+    font-size: 12px;
+  }
+  .wrap .btn {
+    text-align: center;
+  }
+  .wrap .btn button {
+    padding: 15px 45px;
+    border: none;
+    background: #b2ebf2;
+    outline: none;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  .wrap .btn button:last-child {
+    margin-right: 0;
+  }
+`;
 
 const Friend = () => {
-  return <div>Friend</div>;
+  return (
+    <StyledFriend>
+      <h1>친구들 총인원 : </h1>
+      <p className='chk'>
+        <input type='checkbox' checked />
+        보이기/숨기기
+      </p>
+      <FriendList />
+      <p className='btn'>
+        <button>모두삭제</button>
+        <button>초기복구</button>
+      </p>
+    </StyledFriend>
+  );
 };
 
 export default Friend;
