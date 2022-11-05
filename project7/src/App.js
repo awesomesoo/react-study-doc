ba71cc6e382d3137a5373f73c80348d41d56fa87
uiuxsooa <sooa0513@naver.com>
2022-11-05 15:35:51 +0900

    index.js를 사용하여 컴파일 하여 App.js 에서 단축해서 불러와 사용하기

--- project7/src/App.js
+++ project7/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import Todos from './components/todos/Todos';
-import Friend from './components/friend/Friend/Friend';
-import './assets/css/reset.css'
+// import Todos from './components/todos/Todos';
+// import Friend from './components/friend/Friend/Friend';
+
+/*
+ * 작업물이 많을 때는 아래와 같이 쓰면 단축이 된다.
+ */
+import { Friend, Todos } from './components';
+
+import './assets/css/reset.css';
 
 const App = () => {
   return (
     <div>
-       <Todos />
-       <hr/>
-        <Friend />
+      <Todos />
+      <hr />
+      <Friend />
     </div>
   );
 };
---
+++ project7/src/components/index.js
@@ -0,0 +1,3 @@
+// export { 참조변수 } from './폴더명/index.js'
+export { Friend } from './friend';
+export { Todos } from './todos';
