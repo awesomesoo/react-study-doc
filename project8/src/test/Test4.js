9b156d1b7077eb9e4eb0f4f5776cf6c509e65820
uiuxsooa <sooa0513@naver.com>
2022-11-12 11:53:09 +0900

    URL 분리하기

--- project8/src/test/Test4.js
+++ project8/src/test/Test4.js
@@ -3,10 +3,20 @@ import axios from 'axios';
 
 const Test4 = () => {
   const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+
+  /*  URL 분리하기 전
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .then((res) => setPost(res.data));
+  }, []); */
 
   useEffect(() => {
-    axios.get('').then((res) => setPost(res.data));
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    axios.get(url).then((res) => setPost(res.data));
   }, []);
+
   return (
     <div>
       <input type="text" />
