7e094b1308eb378f56c43d484f5c2ef27c61dcdf
uiuxsooa <sooa0513@naver.com>
2022-11-12 10:54:04 +0900

    axios로 api 가져오기

--- project8/src/test/Test1Fetch.js
+++ project8/src/test/Test1Fetch.js
@@ -1,24 +1,68 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Test1Fetch = () => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then((res) => setData(res));
-  }, []);
+  /* 1.
 
-  /* 
 fetch('https://jsonplaceholder.typicode.com/posts')
         .then(
             (res) =>
     console.log(res.json() ) 
-     */
-  // fetch('https://jsonplaceholder.typicode.com/posts')
-  // .then(res => res.json())
-  // .then(res => setData(res))
+    
+    */
+
+  /* 2.
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, []); 
+  
+  */
+
+  /* 3.
+
+  useEffect(() => {
+   axios.get(url)
+   .then( 매개변수 => 매개변수.??)
+  }, []); 
+  
+  */
+
+  /* 3-1.
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => console.log(res));
+  }, []); 
+
+  */
+
+  /* 3-2.
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => console.log(res.data)); // 데이터만 가져옴.
+  }, []);
+
+ */
+
+  /* 3-3.
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => setData(res.data)); // 데이터만 가져옴.
+  }, []); 
+
+  */
+
+  useEffect(() => {}, []);
 
   return (
     <div>
