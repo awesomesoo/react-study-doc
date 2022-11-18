8211f117a875541a84c0fcbd73471e569653567e
uxuisooa <sooa0513@naver.com>
2022-11-18 23:18:26 +0900

    useMemo 사용하기 위한 작업

--- project8/src/useMemoUseCallback/Test8.js
+++ project8/src/useMemoUseCallback/Test8.js
@@ -43,3 +43,49 @@ const Test8 = () => {
 
 export default Test8
 
+/* 
+
+import React, { useState } from 'react';
+import Test8Sub from './Test8Sub';
+
+const Test8 = () => {
+    const [ color , setColor ] = useState('')
+    const [ food , setFood ] = useState('')
+
+    const onColor  = (e) => {
+        setColor( e.target.value )
+    }
+    
+    const onFood  = (e) => {
+        setFood( e.target.value )
+    }
+
+
+    return (
+        <div>
+            <h2>좋아하는 색 </h2>
+            <article>
+                <input type="text" value={color} onChange={ onColor } />
+                <p>red, green, blue</p>
+            </article>
+            <article>
+                <h2> 좋아하는 음식 </h2>
+                <p><input type="radio" name="food" value="불고기" onChange={onFood}/>
+                   <label>불고기</label> 
+                </p>
+                <p><input type="radio" name="food" value="라면" onChange={onFood}/>
+                   <label>라면</label> 
+                </p>
+                <p><input type="radio" name="food" value="쫄면" onChange={onFood}/>
+                   <label>쫄면</label> 
+                </p>
+            </article>
+            <hr/>
+            <Test8Sub color={color} food={food} />
+        </div>
+    );
+};
+
+export default Test8;
+
+*/
---
+++ project8/src/useMemoUseCallbackReview/Test8.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Test8Sub from './Test8Sub'
+
+const Test8 = () => {
+  const [color, setColor] = useState('')
+  const [food, setFood] = useState('')
+
+  const onColor = e => {
+    setColor(e.target.value)
+  }
+
+  const onFood = e => {
+    setFood(e.target.value)
+  }
+
+  return (
+    <>
+      <h2>좋아하는 색</h2>
+      <article>
+        <input type='text' value={color} onChange={onColor} />
+        <p> red, green, blue</p>
+      </article>
+      <h2>좋아하는 음식</h2>
+      <article>
+        <p>
+          <input type='radio' name='food' value='불고기' onChange={onFood} />
+          <label>불고기</label>
+        </p>
+        <p>
+          <input type='radio' name='food' value='라면' onChange={onFood} />
+          <label>라면</label>
+        </p>
+        <p>
+          <input type='radio' name='food' value='쫄면' onChange={onFood} />
+          <label>쫄면</label>
+        </p>
+      </article>
+      <hr />
+      <Test8Sub color={color} food={food} />
+    </>
+  )
+}
+
+export default Test8
+
