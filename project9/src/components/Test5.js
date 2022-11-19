8b40efaebff0851c30c7604f55b779354c97e418
uiuxsooa <sooa0513@naver.com>
2022-11-19 14:28:59 +0900

    리렌더링 방지

--- project9/src/components/Test5.js
+++ project9/src/components/Test5.js
@@ -3,7 +3,8 @@ import Test5Sub from './Test5Sub'
 import Test5Title from './Test5Title'
 /*
  * 여러 컴포넌트에서 여러 일을 수행하고 있다. 하나가 바뀌면 전부다 리턴을 하기 때문에 변경된 값만 리턴해주기위해서 사용한다.
- */
+ * 리렌더링 방지
+ * */
 const Test5 = () => {
   const [name, setName] = useState('홍길동')
   const [age, setAge] = useState(20)
