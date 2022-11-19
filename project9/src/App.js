2f178f417b833acbb3d3b773ee0ddf73c37176fb
uiuxsooa <sooa0513@naver.com>
2022-11-19 11:41:58 +0900

    data 리스트 검색 폼 만들기

--- project9/src/App.js
+++ project9/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
+// import Test1 from './components/Test1'
 // import Gallery from './sample/gallery/Gallery'; // index 만들어서 필요 없음
 import GlobalStyle from './sample/styled/Global'
-// import Test1 from './components/Test1';
-import { Gallery } from './sample'
+// import { Gallery } from './sample'
+import Test2 from './components/Test2'
 
 const App = () => {
   return (
     <>
       {/* <Test1 /> */}
       <GlobalStyle />
-      <Gallery />
+      {/* <Gallery /> */}
+      <Test2 />
     </>
   )
 }
---
+++ project9/src/components/Test2.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+const dataList = [
+  { id: 1, text: 'naver' },
+  { id: 2, text: 'NaVer' },
+  { id: 3, text: 'Daum' },
+  { id: 4, text: 'google' },
+  { id: 5, text: 'GOOGLE' },
+  { id: 6, text: '안녕 홍길동' },
+  { id: 7, text: '주말 강호동' },
+  { id: 8, text: '유재석 진행' },
+  { id: 9, text: 'Nate' },
+  { id: 10, text: 'Dog Cat' },
+  { id: 11, text: '유감가 홍이슬' },
+]
+
+const Test2 = () => {
+  const [data, setData] = useState(dataList)
+  const [text, setText] = useState('')
+
+  return (
+    <>
+      <div>
+        <input type="text" />
+        <button>검색</button>
+      </div>
+      <hr />
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            {item.id} / {item.text}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Test2
+
+/* 
+
+indexOf
+includes
+
+
+
+*/
