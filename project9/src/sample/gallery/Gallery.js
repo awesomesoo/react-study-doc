57c057b7d7d09928dff48a296f91fbdbfbce78d4
uiuxsooa <sooa0513@naver.com>
2022-11-19 10:20:05 +0900

    pixabay 키값 넣기

--- project9/src/sample/gallery/Gallery.js
+++ project9/src/sample/gallery/Gallery.js
@@ -1,45 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import GallerySearch from './GallerySearch';
-import GalleryList from './GalleryList';
-import { GalleryBox } from '../styled/galleryStyle';
- 
+import GallerySearch from './GallerySearch'
+import GalleryList from './GalleryList'
+import { GalleryBox } from '../styled/galleryStyle'
 
 const Gallery = () => {
-    const [ data , setData ] = useState([])  
-    const [ loading , setLoading ] = useState(true)
-    const [ error , setError ] = useState(null)
-    const [ text , setText ] = useState('flowers')
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [text, setText] = useState('flowers')
 
-    useEffect( () => {
-        const API_KEY = '31264567-b2f4129d718fb9580a6f88302'
-        const url = `https://pixabay.com/api/?key=${API_KEY}&q=${text}&image_type=photo`
-        axios.get( url )
-             .then( res  => {
-                setData(res.data.hits)
-                setLoading( false )
-                setError(null)
-             }).catch( error  => {
-                setData([])
-                setLoading( true )
-                setError('주소가 에러')
-             })
-    } ,[text] )
+  useEffect(() => {
+    const API_KEY = '31264567-b2f4129d718fb9580a6f88302'
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${text}&image_type=photo`
+    axios
+      .get(url)
+      .then(res => {
+        setData(res.data.hits)
+        setLoading(false)
+        setError(null)
+      })
+      .catch(error => {
+        setData([])
+        setLoading(true)
+        setError('주소가 에러')
+      })
+  }, [text])
 
-    const onSearch  = ( text ) => {
-        setText( text )         
-    }
+  const onSearch = text => {
+    setText(text)
+  }
 
-    return (
-        <GalleryBox width="1200px">
-            <h2>pixabay - api</h2>            
-            <GallerySearch onSearch={onSearch}  />          
-            <GalleryList data={data} />
-        </GalleryBox>
-    );
-};
+  return (
+    <GalleryBox width="1200px">
+      <h2>pixabay - api</h2>
+      <GallerySearch onSearch={onSearch} />
+      <GalleryList data={data} />
+    </GalleryBox>
+  )
+}
 
-export default Gallery;
+export default Gallery
 /* 
 react-icons 설치
 
