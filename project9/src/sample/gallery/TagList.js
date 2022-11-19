f5e5292771626df6d3a8089d9537a97e95042a84
uiuxsooa <sooa0513@naver.com>
2022-11-19 10:30:37 +0900

    taglist 컴포넌트에 react-icons 사용

--- project9/src/sample/gallery/TagList.js
+++ project9/src/sample/gallery/TagList.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { TagListBox } from '../styled/galleryStyle'
+// https://react-icons.github.io/react-icons/icons?name=ai
+import { AiFillCloseCircle } from 'react-icons/ai'
 
 const TagList = () => {
   return (
     <TagListBox>
       <button>삭제</button>
       <ul>
-        <li>검색어 <span></span></li>
+        <li>
+          검색어
+          <span>
+            <AiFillCloseCircle />
+          </span>
+        </li>
       </ul>
     </TagListBox>
   )
