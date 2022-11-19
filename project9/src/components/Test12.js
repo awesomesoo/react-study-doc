347d59237613800f3e7d02d9232b8646fb053779
uiuxsooa <sooa0513@naver.com>
2022-11-19 17:24:58 +0900

    axios hook으로 리스트 조회, 로딩, 에러 처리

--- project9/src/components/Test12.js
+++ project9/src/components/Test12.js
@@ -2,18 +2,30 @@ import React from 'react'
 import useAxios from '../hooks/useAxios'
 
 const Test12 = () => {
-  const { data } = useAxios('https://jsonplaceholder.typicode.com/posts')
+  const { data, loading, error } = useAxios(
+    'https://jsonplaceholder.typicode.com/posts',
+  )
   return (
     <>
+      {loading && <h1>loading...</h1>}
       <ul>
-        {data.map(item => (
-          <li key={item.id}>
-            {item.id} / {item.title}
-          </li>
-        ))}
+        {data &&
+          !loading &&
+          data.map(item => (
+            <li key={item.id}>
+              {item.id} / {item.title}
+            </li>
+          ))}
       </ul>
+      {/* 주소가 잘못되면 메시지 노출 */}
+      <h3>{error ? error : null}</h3>
     </>
   )
 }
 
 export default Test12
+
+/* 
+ 패치보다 엑시오스를 추천한다.
+ 패치는 어싱크 어웨잇으로 강제로 에러를 만들어 오류를 찾아가야한다.
+*/
--- project9/src/hooks/useAxios.js
+++ project9/src/hooks/useAxios.js
@@ -3,11 +3,22 @@ import axios from 'axios'
 
 export const useAxios = url => {
   const [data, setData] = useState([])
+  const [loading, setLoding] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    axios.get(url).then(res => setData(res.data))
+    axios
+      .get(url)
+      .then(res => {
+        setData(res.data)
+        setLoding(false)
+        setError(null)
+      })
+      .catch(error => {
+        setError('주소를 찾지 못합니다.')
+      })
   }, [url])
-  return { data }
+  return { data, loading, error }
 }
 
 export default useAxios
