1170cd5d241fc60805d3a432d2a324674ca4e159
uiuxsooa <sooa0513@naver.com>
2022-11-26 11:38:55 +0900

    Context를 사용한 Counter 완성

--- project10/src/contexts/CounterContext.js
+++ project10/src/contexts/CounterContext.js
@@ -4,13 +4,16 @@ import React, { createContext, useState } from 'react'
 export const CounterContext = createContext()
 
 const CounterProvider = props => {
+  // 상태변수 선언
   const [count, setCount] = useState(1)
+  // 행동의 함수 등 모든 실행문을 작성함
   const onUp = () => {
     setCount(count + 1)
   }
   const onDown = () => {
     setCount(count - 1)
   }
+  // Provider는 제공자
   return (
     <CounterContext.Provider value={{ count, onUp, onDown }}>
       {props.children}
