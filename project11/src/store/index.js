a6af8a1f10f75f2ac54663e547d966d4019f34dd
uiuxsooa <sooa0513@naver.com>
2022-11-26 17:17:30 +0900

    store index.js count 추가

--- project11/src/store/index.js
+++ project11/src/store/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
-import color from './modules/color.js'
+import color from './modules/color'
+import count from './modules/count'
 
 export default combineReducers({
-  colorA: color,
+  // 키값:
+  colorA: color, // colorA: color를 color 라고 써도 된다.
+  count,
 })
