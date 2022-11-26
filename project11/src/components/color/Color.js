4641cb6431f825ba31791a1f436735e313dbd168
uiuxsooa <sooa0513@naver.com>
2022-11-26 16:36:51 +0900

    useSelector

--- project11/src/components/color/Color.js
+++ project11/src/components/color/Color.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Color = () => {
+  const color = useSelector(state => state.colorA.color) // state 를 쓰면 모든걸 가져온다.
+  // useSelector(state => state.colorA.color)
+  // colorA 는 초기변수
   return (
     <div>
-      <h2 style={{ color: 'orange' }}>컬러 : </h2>
+      <h2 style={{ color: color }}>컬러 : {color}</h2>
       <p>
         <button>red</button>
         <button>green</button>
