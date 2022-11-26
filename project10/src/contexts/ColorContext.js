c8dcdc8ea5e31751124101304d1302ab154a24ae
uiuxsooa <sooa0513@naver.com>
2022-11-26 15:20:13 +0900

    CounterContext 사용, ColorProvider에 useMemo 사용

--- project10/src/contexts/ColorContext.js
+++ project10/src/contexts/ColorContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useMemo, useState } from 'react'
 
 // 2. 여기에는 모든 상태값, 함수 등을 작성한다.
 // 모든 행동 실행문으로 작성한다.
@@ -21,8 +21,17 @@ const ColorProvider = props => {
   const onSkyblue = () => {
     setColor('skyblue')
   }
+
+  // ★ Context를 사용할때 꼭 useMemo를 사용해야한다. 변경되는 값 외에 다른것도 다 실행되기 떄문에
+  // ★ useMemo( () => (), [])
+  // 콜백도 쓰라고 경고가 뜰 것이다.
+  const value = useMemo(
+    () => ({ color, onRed, onGreen, onPink, onSkyblue }),
+    [color, onRed, onGreen, onPink, onSkyblue],
+  )
+
   return (
-    <ColorContext.Provider value={{ color, onRed, onGreen, onPink, onSkyblue }}>
+    <ColorContext.Provider value={value}>
       {/* <Color/> UI 디자인이 이 안에 들어오는 것이다. */}
       {props.children}
     </ColorContext.Provider>
