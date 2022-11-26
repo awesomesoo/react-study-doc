03b071029deca04ee73c865ba85f27359f1f6661
uiuxsooa <sooa0513@naver.com>
2022-11-26 11:34:01 +0900

    Counter 컴포넌트에서 useContext 사용

--- project10/src/components/count/Counter.js
+++ project10/src/components/count/Counter.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CounterContext } from '../../contexts/CounterContext'
 
 const Counter = () => {
+  // const {Provider 의 value }   = useContext(데이터관리자)
+  const { count, onUp, onDown } = useContext(CounterContext)
   return (
     <div>
-      <h2>카운터 : </h2>
-      <button>증가</button>
-      <button>감소</button>
+      <h2>카운터 : {count}</h2>
+      <button onClick={onUp}>증가</button>
+      <button onClick={onDown}>감소</button>
     </div>
   )
 }
