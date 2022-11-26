c55a51044eb8b08a9d8a1528d20e88306df543a9
uiuxsooa <sooa0513@naver.com>
2022-11-26 16:39:27 +0900

    useDispatch

--- project11/src/components/color/Color.js
+++ project11/src/components/color/Color.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { red, green, pink, blue } from '../../store/modules/color'
 
 const Color = () => {
   const color = useSelector(state => state.colorA.color) // state 를 쓰면 모든걸 가져온다.
   // useSelector(state => state.colorA.color)
   // colorA 는 초기변수
+
+  const dispath = useDispatch()
   return (
     <div>
       <h2 style={{ color: color }}>컬러 : {color}</h2>
       <p>
-        <button>red</button>
-        <button>green</button>
-        <button>blue</button>
-        <button>pink</button>
+        <button onClick={() => dispath(red())}>red</button>
+        <button onClick={() => dispath(green())}>green</button>
+        <button onClick={() => dispath(blue())}>blue</button>
+        <button onClick={() => dispath(pink())}>pink</button>
       </p>
     </div>
   )
