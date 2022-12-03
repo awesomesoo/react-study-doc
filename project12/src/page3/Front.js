2239d3c0b37f190cd5039438ef000dcdd595e833
uiuxsooa <sooa0513@naver.com>
2022-12-03 12:12:03 +0900

    useParams 이용해서 사용자 정의 변수 받기 주석

--- project12/src/page3/Front.js
+++ project12/src/page3/Front.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 
 const Front = ({ data }) => {
   // 사용자 정의변수 받기
-  // const {사용자 정의변수} = useParams()
+  // const {사용자 정의변수} = useParams()   :  값 받기
   // useParams() 는 리액트라우터돔에서 지원해준다
   // 이 방법은 제일 안좋은 방법이다. 참고만 하자
   const { name } = useParams()
