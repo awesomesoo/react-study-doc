f803d91809c05e238ff4e8f13b7db651dc77cf4c
uiuxsooa <sooa0513@naver.com>
2022-12-03 14:50:28 +0900

    navigate 생성후 적용

--- project12/src/page5/Member.js
+++ project12/src/page5/Member.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Member = ({ item }) => {
   const { name, email, phone, id } = item
+
+  const navigate = useNavigate()
 
   const css = {
     margin: 20,
     padding: 15,
     border: '1px solid #999',
+  }
+
+  const onGo = () => {
+    navigate(`/member/${id}`)
+    // navigate(상대경로, 절대경로, 숫자(양수/음수)) - 1 다음페이지, -1 이전페이지
   }
   return (
     <div style={css}>
@@ -18,7 +25,7 @@ const Member = ({ item }) => {
       <p>
         <Link to={`/member/${id}`}>상세보기+</Link>
       </p>
-      <button>자세히보기</button>
+      <button onClick={onGo}>자세히보기</button>
     </div>
   )
 }
