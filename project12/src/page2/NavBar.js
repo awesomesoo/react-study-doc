a6b5d400d0ebe553ba418a3bd3893876e0a79fca
uiuxsooa <sooa0513@naver.com>
2022-12-03 11:36:28 +0900

    NavBar 제작

--- project12/src/page2/NavBar.js
+++ project12/src/page2/NavBar.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import data from './navData'
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false)
+
   const onToggle = () => {
     setIsOpen(!isOpen)
   }
@@ -16,6 +18,49 @@ const NavBar = () => {
       {/* 클래스가 하나밖에 없으면 아래와 같이 쓰지만 
       두개 이상이기에 템플릿 리터럴을 사용해준다. */}
       {/* <nav className={isOpen ? 'on' : ''}> */}
+      <nav className={`nav ${isOpen ? 'on' : ''} `}>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              <Link to={item.path}>{item.title}</Link>
+            </li>
+          ))}
+          {/* <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/ceo">Ceo</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li> */}
+        </ul>
+        <p className="close" onClick={() => setIsOpen(false)}>
+          X
+        </p>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
+/* 
+
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onToggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div className="navbar">
+      <p className="all-menu" onClick={onToggle}>
+        menu
+      </p>
       <nav className={`nav ${isOpen ? 'on' : ''} `}>
         <ul>
           <li>
@@ -37,3 +82,6 @@ const NavBar = () => {
 }
 
 export default NavBar
+
+
+*/
