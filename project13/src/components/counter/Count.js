58f8dcf7b88eac6e12c9e304e7e131936e01ba30
uiuxsooa <sooa0513@naver.com>
2022-12-03 17:07:11 +0900

    dispatch 연결

--- project13/src/components/counter/Count.js
+++ project13/src/components/counter/Count.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  decrementByAmount,
+} from '../../store/modules/count'
 
 const Count = () => {
+  const value = useSelector(state => state.countPage.value)
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <h2>숫자 : </h2>
+      <h2>숫자 : {value}</h2>
       <p>
-        <button>증가</button>
-        <button>감소</button>
-        <button>10증가</button>
-        <button>10감소</button>
+        <button onClick={() => dispatch(increment())}>증가</button>
+        <button onClick={() => dispatch(decrement())}>감소</button>
+        <button onClick={() => dispatch(incrementByAmount(10))}>10증가</button>
+        <button onClick={() => dispatch(decrementByAmount(10))}>10감소</button>
       </p>
     </div>
   )
