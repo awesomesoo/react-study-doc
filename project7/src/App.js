3c545ac232ea7748259b80b35199cab4f0d7ba04
uiuxsooa <sooa0513@naver.com>
2023-01-27 02:19:02 +0900

    project7 app.js 수정

--- project7/src/App.js
+++ project7/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from 'react'
 // import Todos from './components/todos/Todos';
 // import Friend from './components/friend/Friend/Friend';
 
 /*
  * 작업물이 많을 때는 아래와 같이 쓰면 단축이 된다.
  */
-// import { Friend, Todos } from './components';
-// import { Friend, Todos } from './componentsReview';
-import Test from './sampleTest';
+import { Friend, Todos } from './components'
 
-import './assets/css/reset.css';
+import './assets/css/reset.css'
 
 const App = () => {
   return (
     <div>
-      <Test />
-      {/* <Todos /> */}
-      {/* <hr /> */}
-      {/* <Friend /> */}
+      <Todos />
+      <hr />
+      <Friend />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
